778874b1e616c078f6b423e65acb076e79c0df5b
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2021-11-24 19:15:54 -0300

    project of noticias, before auth with passport

---
+++ routes/restrito.js
@@ -0,0 +1,24 @@
+const express = require('express')
+
+const router = express.Router()
+
+const Noticia = require('../models/noticias')
+
+router.use((req, res, next) => {
+  if ('user' in req.session) {
+    if (req.session.user.roles.indexOf('restrito') >= 0) return next()
+    else res.redirect('/')
+
+  }
+  res.redirect('/login')
+})
+
+
+router.get('/', (req, res) => res.send('restrito'))
+
+router.get('/noticias', async (req, res) => {
+  const noticias = await Noticia.find({ category: 'private' })
+  res.render('noticias/restrito', { noticias })
+})
+
+module.exports = router
